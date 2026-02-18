babb69dcf18c89f481c33a23b0542a52e5ab3d28
seniority1 <quicktaskteam2025@gmail.com>
2026-02-18 22:22:57 +0100

    Fix formatting in User schema password field

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true },
-    password: { type: String, required: true }, // This will be hashed!
+    password: { type: String, required: true },
     date: { type: Date, default: Date.now }
 });
 
